fd194286e8fca369607a7ce6945fa219a1cca7fb
shinixsensei <crezoxplays@gmail.com>
2021-08-15 17:59:38 +0200

    accept all domains

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.use(express.json())
 
-const productionDomain = 'https://4c3711.xyz';
+const productionDomain = 'https://traktorraces24.4c3711.xyz';
 const testingDomain = 'http://localhost:3000';
 const domain = productionDomain;
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', domain);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
